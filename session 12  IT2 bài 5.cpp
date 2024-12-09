7c8408e4e747079e93ef2ca23979d60f2866bb58
hyuuhoang <Huyhoangzz2k5@gmail.com>
2024-12-09 15:35:01 +0700

    nguyen huy hoang

---
+++ session 12  IT2 bài 5.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+
+int kiemTraSoNguyenTo(int n) {
+    if (n < 2) return 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int so1, so2;
+
+    printf("nhap so nguyen thu nhat: ");
+    scanf("%d", &so1);
+    printf("nhap so nguyen thu hai: ");
+    scanf("%d", &so2);
+
+    
+    if (kiemTraSoNguyenTo(so1)) {
+        printf("%d la so nguyen to.\n", so1);
+    } else {
+        printf("%d khong phai la so nguyen to.\n", so1);
+    }
+
+    if (kiemTraSoNguyenTo(so2)) {
+        printf("%d la so nguyen to.\n", so2);
+    } else {
+        printf("%d khong phai so nguyen to.\n", so2);
+    }
+
+    return 0;
+}
+
